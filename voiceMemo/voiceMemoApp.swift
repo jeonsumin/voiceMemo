9e00db36d9859b18e6fba563a0e1ed86553ba336
jeonsumin <soomis7@naver.com>
2023-10-11 09:47:48 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by Terry on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
